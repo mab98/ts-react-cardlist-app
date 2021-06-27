2149a8aa4a5646dd3152e1cf216fe3b2de4bef67
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-06-27 23:09:40 +0500

    till addUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  interface UserInterface {
+    name: string;
+    age: number;
+    job: string;
+  }
+
+  const [user, setUser] = useState<UserInterface>({ name: '', age: 0, job: '' });
+  const [users, setUsers] = useState<UserInterface[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const addUser = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    setUsers([...users, user]);
+  }
+
+  React.useEffect(() => {
+    console.log(...users);
+  }, [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React with Typescript</h1>
+      <form onSubmit={addUser}>
+        <label htmlFor="username">Name:</label> <br />
+        <input type="text" name="name" id="username" value={user.name} onChange={handleChange} /> <br />
+        <label htmlFor="userAge">Age:</label> <br />
+        <input type="number" name="age" id="userAge" value={user.age} placeholder='enter age' onChange={handleChange} /> <br />
+        <label htmlFor="userJob">Job:</label> <br />
+        <input type="text" name="job" id="userJob" value={user.job} onChange={handleChange} /> <br />
+        <button type="submit">Add User</button>
+      </form>
     </div>
   );
 }
